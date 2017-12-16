0a8af866be48ca537df587f9bb5d513c02a984ac
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-12-16 09:00:13 -0500

    all projects compiling

---
+++ src/LagoVista.DeviceManager.Android/SplashScreen.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Content.PM;
+using LagoVista.DeviceManager.Droid;
+
+namespace LagoVista.Droid.Simulator
+{
+    [Activity(Label = "Device Mnager", MainLauncher =true, NoHistory = true, Theme = "@style/Theme.Splash", ConfigurationChanges = ConfigChanges.ScreenLayout | ConfigChanges.Orientation )]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+        }
+    }
+}
